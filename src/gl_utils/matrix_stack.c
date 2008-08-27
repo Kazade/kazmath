e54e0e33810b0fc84ceed6da1c36c50d9b9763fa
Luke Benstead (none) <luke@helium>
2008-08-27 13:33:12 +0100

    Added the initial gl_utils implementation for replacing the matrix functionality deprecated in OpenGL 3.0

---
+++ src/gl_utils/matrix_stack.c
@@ -0,0 +1,22 @@
+
+#include "matrix_stack.h"
+
+static kmMat4 modelview_matrix;
+static kmMat4 projection_matrix;
+
+
+void kmGLMatrixMode(MATRIX_TYPE type) {
+
+}
+
+void kmGLPushMatrix(void) {
+
+}
+
+void kmGLPopMatrix(void) {
+
+}
+
+void kmGLLoadIdentity(void) {
+
+}
---
+++ src/gl_utils/matrix_stack.h
@@ -0,0 +1,15 @@
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+
+enum MATRIX_TYPE {
+	KM_GL_MODELVIEW,
+	KM_GL_PROJECTION,
+	KM_GL_TEXTURE
+};
+
+void kmGLPushMatrix(void);
+void kmGLPopMatrix(void);
+void kmGLMatrixMode(MATRIX_TYPE type);
+void kmGLLoadIdentity(void);
+
+#endif // STACK_H_INCLUDED
