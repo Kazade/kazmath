a49457c88961241a5348ab037a7c01f490819599
Luke Benstead <kazade@gmail.com>
2009-04-26 09:49:17 +0100

    Added stub for kmMat3RotationToAxisAngle()

--- src/mat3.c
+++ src/mat3.c
@@ -280,3 +280,8 @@ kmMat3* kmMat3RotationAxis(kmMat3* pOut, const struct kmVec3* axis, kmScalar rad
 
     return pOut;    
 }
+
+kmVec3* kmMat3RotationToAxisAngle(kmVec* pAxis, kmScalar radians, const kmMat3* pIn) 
+{
+    return pAxis;
+}
--- src/mat3.h
+++ src/mat3.h
@@ -49,7 +49,10 @@ kmMat3* kmMat3Transpose(kmMat3* pOut, const kmMat3* pIn);
 kmScalar kmMat3Determinant(const kmMat3* pIn);
 kmMat3* kmMat3Multiply(kmMat3* pOut, const kmMat3* pM1, const kmMat3* pM2);
 kmMat3* kmMat3ScalarMultiply(kmMat3* pOut, const kmMat3* pM, const kmScalar pFactor);
+
 kmMat3* kmMat3RotationAxis(kmMat3* pOut, const struct kmVec3* axis, kmScalar radians);
+kmVec3* kmMat3RotationToAxisAngle(kmVec* pAxis, kmScalar radians, const kmMat3* pIn);
+
 kmMat3* kmMat3Assign(kmMat3* pOut, const kmMat3* pIn);
 int  kmMat3AreEqual(const kmMat3* pM1, const kmMat3* pM2);
 
