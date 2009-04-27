66912a8516fff44562315e9e8092fbd4cfa9e75c
Luke Benstead <kazade@gmail.com>
2009-04-27 18:54:48 +0100

    Added a test for kmMat3Identity

--- kazmath/tests/test_mat3.cpp
+++ kazmath/tests/test_mat3.cpp
@@ -85,3 +85,14 @@ BOOST_AUTO_TEST_CASE(test_mat3_axis_angle)
 
 	//TODO: Finish this
 }
+
+BOOST_AUTO_TEST_CASE(test_mat3_identity)
+{
+	float identity[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+	kmMat3 expected, actual;
+	kmMat3Fill(&expected, identity);
+	kmMat3Identity(&actual);
+
+	BOOST_CHECK(kmMat3AreEqual(&expected, &actual));
+}
+
