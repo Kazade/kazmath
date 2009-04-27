6f28a790fcc2de31243e8305b7563b3b9b3d594b
Luke Benstead <kazade@gmail.com>
2009-04-27 10:37:34 +0100

    Added stub mat3 test file

---
+++ kazmath/tests/test_mat3.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE test_mat3
+
+#include <boost/bind.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+BOOST_AUTO_TEST_CASE(test_stub)
+{
+
+}
