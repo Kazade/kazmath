23d56b82637ab541011cbf78ac5f67cda21b3a7f
Luke Benstead <kazade@gmail.com>
2009-04-28 08:49:59 +0100

    Added a commented test for kmMat4Inverse, however kmMat4Adjugate and kmMat4Determinate need implementing

--- kazmath/tests/test_mat4.cpp
+++ kazmath/tests/test_mat4.cpp
@@ -3,7 +3,18 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 
-BOOST_AUTO_TEST_CASE(test_stub)
-{
+#include "mat4.h"
 
-}
+/*
+BOOST_AUTO_TEST_CASE(test_mat4_inverse)
+{
+	kmMat4 mat;
+	BOOST_CHECK(NULL != kmMat4Identity(&mat));
+
+	kmMat4 adj;
+	BOOST_CHECK(NULL != kmMat4Adjugate(&adj, &mat));
+	BOOST_CHECK(NULL != kmMat4Inverse(&mat, kmMat4Determinant(&mat), &mat));
+	BOOST_CHECK(kmMat4IsIdentity(&mat));
+}*/
+
+
