0e827a9806110d0e390e63015641cee3f58e3e31
Luke Benstead <kazade@gmail.com>
2009-08-01 09:20:33 +0100

    Fixed some whitespace issues in plane.c

--- kazmath/src/plane.c
+++ kazmath/src/plane.c
@@ -32,44 +32,44 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 const kmScalar kmPlaneDot(const kmPlane* pP, const kmVec4* pV)
 {
-	//a*x + b*y + c*z + d*w
+    //a*x + b*y + c*z + d*w
 
-	return (pP->a * pV->x +
-			pP->b * pV->y +
-			pP->c * pV->z +
-			pP->d * pV->w);
+    return (pP->a * pV->x +
+	    pP->b * pV->y +
+	    pP->c * pV->z +
+	    pP->d * pV->w);
 }
 
 const kmScalar kmPlaneDotCoord(const kmPlane* pP, const kmVec3* pV)
 {
-	return (pP->a * pV->x +
-			pP->b * pV->y +
-			pP->c * pV->z + pP->d);
+    return (pP->a * pV->x +
+	    pP->b * pV->y +
+	    pP->c * pV->z + pP->d);
 }
 
 const kmScalar kmPlaneDotNormal(const kmPlane* pP, const kmVec3* pV)
 {
-	return (pP->a * pV->x +
-			pP->b * pV->y +
-			pP->c * pV->z);
+    return (pP->a * pV->x +
+	    pP->b * pV->y +
+	    pP->c * pV->z);
 }
 
 kmPlane* const kmPlaneFromPointNormal(kmPlane* pOut, const kmVec3* pPoint, const kmVec3* pNormal)
 {
-	/*
-		Planea = Nx
-		Planeb = Ny
-		Planec = Nz
-		Planed = −N⋅P
-	*/
+    /*
+	Planea = Nx
+	Planeb = Ny
+	Planec = Nz
+	Planed = −N⋅P
+    */
 
 
-	pOut->a = pNormal->x;
-	pOut->b = pNormal->y;
-	pOut->c = pNormal->z;
-	pOut->d = -kmVec3Dot(pNormal, pPoint);
+    pOut->a = pNormal->x;
+    pOut->b = pNormal->y;
+    pOut->c = pNormal->z;
+    pOut->d = -kmVec3Dot(pNormal, pPoint);
 
-	return pOut;
+    return pOut;
 }
 
 /**
@@ -78,52 +78,52 @@ kmPlane* const kmPlaneFromPointNormal(kmPlane* pOut, const kmVec3* pPoint, const
  */
 kmPlane* const kmPlaneFromPoints(kmPlane* pOut, const kmVec3* p1, const kmVec3* p2, const kmVec3* p3)
 {
-	/*
-	v = (B − A) × (C − A)
-	n = 1⁄|v| v
-	Outa = nx
-	Outb = ny
-	Outc = nz
-	Outd = −n⋅A
-	*/
+    /*
+    v = (B − A) × (C − A)
+    n = 1⁄|v| v
+    Outa = nx
+    Outb = ny
+    Outc = nz
+    Outd = −n⋅A
+    */
 
-	kmVec3 n, v1, v2;
-	kmVec3Subtract(&v1, p2, p1); //Create the vectors for the 2 sides of the triangle
-	kmVec3Subtract(&v2, p3, p1);
-	kmVec3Cross(&n, &v1, &v2); //Use the cross product to get the normal
+    kmVec3 n, v1, v2;
+    kmVec3Subtract(&v1, p2, p1); //Create the vectors for the 2 sides of the triangle
+    kmVec3Subtract(&v2, p3, p1);
+    kmVec3Cross(&n, &v1, &v2); //Use the cross product to get the normal
 
-	kmVec3Normalize(&n, &n); //Normalize it and assign to pOut->m_N
+    kmVec3Normalize(&n, &n); //Normalize it and assign to pOut->m_N
 
-	pOut->a = n.x;
-	pOut->b = n.y;
-	pOut->c = n.z;
-	pOut->d = kmVec3Dot(kmVec3Scale(&n, &n, -1.0), p1);
+    pOut->a = n.x;
+    pOut->b = n.y;
+    pOut->c = n.z;
+    pOut->d = kmVec3Dot(kmVec3Scale(&n, &n, -1.0), p1);
 
-	return pOut;
+    return pOut;
 }
 
 kmVec3* const kmPlaneIntersectLine(kmVec3* pOut, const kmPlane* pP, const kmVec3* pV1, const kmVec3* pV2)
 {
-	/*
-		n = (Planea, Planeb, Planec)
-		d = V − U
-		Out = U − d⋅(Pd + n⋅U)⁄(d⋅n) [iff d⋅n ≠ 0]
-	*/
+    /*
+	    n = (Planea, Planeb, Planec)
+	    d = V − U
+	    Out = U − d⋅(Pd + n⋅U)⁄(d⋅n) [iff d⋅n ≠ 0]
+    */
     kmVec3 d;
-	assert(0 && "Not implemented");
+    assert(0 && "Not implemented");
 
 
-	kmVec3Subtract(&d, pV2, pV1); //Get the direction vector
+    kmVec3Subtract(&d, pV2, pV1); //Get the direction vector
 
 
-	//TODO: Continue here!
-	/*if (fabs(kmVec3Dot(&pP->m_N, &d)) > kmEpsilon)
-	{
-		//If we get here then the plane and line are parallel (i.e. no intersection)
-		pOut = nullptr; //Set to nullptr
+    //TODO: Continue here!
+    /*if (fabs(kmVec3Dot(&pP->m_N, &d)) > kmEpsilon)
+    {
+	    //If we get here then the plane and line are parallel (i.e. no intersection)
+	    pOut = nullptr; //Set to nullptr
 
-		return pOut;
-	} */
+	    return pOut;
+    } */
 
     return NULL;
 }
